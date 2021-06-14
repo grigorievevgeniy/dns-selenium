d9462ac0f99e9ef9e98e0d29aac588b7da0bc678
Grigoriev <timeinvest27@gmail.com>
2021-06-14 11:38:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dns_selenium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
